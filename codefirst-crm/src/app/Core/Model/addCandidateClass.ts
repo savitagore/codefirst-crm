e510651976e9a566102ccbc684ff2c1f3de807dc
Kshitija Gaikwad <kshitijagaikwad1998@gmail.com>
2024-01-12 18:42:33 +0530

    sideNav Menu added

---
+++ codefirst-crm/src/app/Core/Model/addCandidateClass.ts
@@ -0,0 +1,33 @@
+export class addCandidate{
+    
+        candidateId: number
+        name: string
+        contactNo: string
+        email: string
+        password: string
+        createdDate: string
+        city: string
+        collegeName: string
+        education: string
+        bankName: string
+        accNo: string
+        ifscCode: string
+        reference: string
+
+        constructor(){
+            this.candidateId=0;
+            this.name= ""
+            this.contactNo= ""
+            this.email= ""
+            this.password= ""
+            this.createdDate= ""
+            this.city= ""
+            this.collegeName= ""
+            this.education= ""
+            this.bankName= ""
+            this.accNo= ""
+            this.ifscCode= ""
+            this.reference= ""
+        }
+      
+}
